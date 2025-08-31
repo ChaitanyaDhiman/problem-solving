6c31476fc881c4d4779c5ca8e88c7bbdce36995b
ChaitanyaDhiman <chaitanya.dhiman292@gmail.com>
2025-08-31 13:39:02 +0530

    Adding more Arrays and Strings problems.

---
+++ src/main/java/com/app/arraysandstrings/Problem3.java
@@ -0,0 +1,107 @@
+package com.app.arraysandstrings;
+
+//    Maximum product of a triplet (subsequence of size 3) in array
+//
+//    Given an integer array, find a maximum product of a triplet in the array.
+//
+//    Examples:
+//    Input:  arr[ ] = [10, 3, 5, 6, 20]
+//    Output: 1200
+//    Explanation: Multiplication of 10, 6 and 20
+//
+//    Input:  arr[ ] =  [-10, -3, -5, -6, -20]
+//    Output: -90
+//
+//    Input: arr[ ] =  [1, -4, 3, -6, 7, 0]
+//    Output: 168
+
+import java.util.Arrays;
+
+public class Problem3 {
+
+//    By Using three nested loops
+//    A simple solution is to check for every triplet using three nested loops.
+//    Time Complexity: O(n3)
+    public static int maxProductOfThreeApproach1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int length = arr.length;
+        int maxProduct = Integer.MIN_VALUE;
+
+        for (int i = 0; i < length - 2; i++) {
+            for (int j = i + 1; j < length - 1; j++) {
+                for (int k = j + 1; k < length; k++) {
+                    maxProduct = Math.max(maxProduct, arr[i] * arr[j] * arr[k]);
+                }
+            }
+        }
+
+        return maxProduct;
+    }
+
+//    By Using sorting - Time O(n*log(n)) and Space O(1)
+//    - Sort the array using some efficient in-place sorting algorithm in ascending order.
+//    - In triplets, either there will be 2 negative elements and 1 positive element or all 3 positive elements so that resultant product will be positive.
+//    - Therefore, To maximise the result return the maximum of  product of the last three elements of the array and the product of the first two elements and last element.
+    public static int maxProductOfThreeApproach2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int length = arr.length;
+
+        Arrays.sort(arr);
+
+//      return arr[length-1] * arr[length - 2] * arr[length - 3]; -> In-case of negative elements this won't work.
+        return Math.max(arr[0] * arr[1] * arr[length - 1], arr[length-1] * arr[length - 2] * arr[length - 3]);
+    }
+
+//    By Using Greedy approach - Time O(n) and Space O(1)
+//    Scan the array and compute the Maximum, second maximum and third maximum element present in the array.
+//    Scan the array and compute Minimum and second minimum element present in the array.
+//    Return the maximum of product of Maximum, second maximum and third maximum and product of Minimum, second minimum and Maximum element.
+    public static int maxProductOfThreeApproach3(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        int length = arr.length;
+
+        int maxFirst = Integer.MIN_VALUE, maxSecond = Integer.MIN_VALUE, maxThird = Integer.MIN_VALUE;
+        int minFirst = Integer.MAX_VALUE, minSecond = Integer.MAX_VALUE;
+
+        for(int i : arr){
+            if(i > maxFirst){
+                maxThird = maxSecond;
+                maxSecond = maxFirst;
+                maxFirst = i;
+            } else if(i > maxSecond){
+                maxThird = maxSecond;
+                maxSecond = i;
+            } else if(i > maxThird){
+                maxThird = i;
+            }
+
+            if(i < minFirst){
+                minSecond = minSecond;
+                minFirst = i;
+            } else if(i < minSecond){
+                minSecond = i;
+            }
+        }
+
+        return Math.max(maxFirst * maxSecond * maxThird, minFirst * minSecond * maxThird);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 3, 5, 6, 20 };
+        int[] arr2 = { -10, 3, 5, 6, 20 };
+
+        System.out.println(maxProductOfThreeApproach1(arr)); // Output: 1200
+        System.out.println(maxProductOfThreeApproach2(arr)); // Output: 1200
+        System.out.println(maxProductOfThreeApproach2(arr2)); // Output : 600
+        System.out.println(maxProductOfThreeApproach3(arr)); // Output : 1200
+    }
+}
---
+++ src/main/java/com/app/arraysandstrings/Problem4.java
@@ -0,0 +1,93 @@
+package com.app.arraysandstrings;
+
+//      Maximum consecutive one’s (or zeros) in a binary array
+//
+//      Given a binary array arr[] consisting of only 0s and 1s, find the length of the longest contiguous sequence of either 1s or 0s in the array.
+//
+//      Examples :
+//      Input: arr[] = [0, 1, 0, 1, 1, 1, 1]
+//      Output: 4
+//      Explanation: The maximum number of consecutive 1’s in the array is 4 from index 3-6.
+//
+//      Input: arr[] = [0, 0, 1, 0, 1, 0]
+//      Output: 2
+//      Explanation: The maximum number of consecutive 0’s in the array is 2 from index 0-1.
+//
+//      Input: arr[] = [0, 0, 0, 0]
+//      Output: 4
+//      Explanation: The maximum number of consecutive 0’s in the array is 4.
+
+public class Problem4 {
+
+//    Using Simple Traversal - O(n) Time and O(1) Space
+//    The idea is to traverse the array once and count how many times the same value repeats consecutively.
+//    The thought process is to use a counter that increases when the current element matches the previous one.
+//    If a change is detected, we update the maximum streak and reset the count.
+//
+//    Steps-By-Step Approach:
+//    - Initialize two variables: maxCount = 0 to store result and count = 1 for current streak length.
+//    - Iterate through the array starting from index 1 to compare each element with the previous one.
+//    - If arr[i] equals arr[i - 1], it means the streak continues, so increment count by 1.
+//    - If they differ, update maxCount with the current count and reset count = 1 for a new streak.
+//    - Continue this comparison until the loop finishes checking all elements in the array.
+//    - After the loop, take the maximum of maxCount and count to cover the last streak.
+//    - Return maxCount which holds the maximum number of consecutive 1s or 0s in the array.
+
+    public static int maxConsecutiveOccurrencesApproach1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int length = arr.length;
+        int maxCount = 0, count = 1;
+
+        for (int i = 1; i < length ; i++) {
+            if(arr[i] == arr[i - 1]){
+                count++;
+            } else {
+                maxCount = Math.max(maxCount, count);
+                count = 1;
+            }
+        }
+        return Math.max(maxCount, count);
+    }
+
+//    Using Bit Manipulation - O(n) Time and O(1) Space
+//    The idea is to use XOR (^) to check if consecutive elements are the same.
+//    As XOR of two numbers is 0 if both numbers are same. So, If prev ^ num == 0,
+//    the sequence continues; else we update the maximum streak and reset the count.
+//
+//    Steps to implement the above idea:
+//    - Initialize three variables: maxCount = 0, count = 0, and prev = -1 to track current and previous bits.
+//    - Traverse the array, use XOR to compare current num with prev, if equal, increment count for the current streak.
+//    - If prev and num differ, update maxCount with the current count, then reset count to 1.
+//    - Update prev to current num in every iteration to track the previous element for the next comparison.
+//    - After loop ends, compare and return the maximum of maxCount and count to cover last streak.
+    public static int maxConsecutiveOccurrencesApproach2(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+
+        int maxCount = 0, count = 0, prev = -1;
+
+        for (int i : arr){
+            if((prev ^ i) == 0){
+                count++;
+            } else {
+                maxCount = Math.max(maxCount, count);
+                count = 1;
+            }
+            prev = i;
+        }
+        return Math.max(maxCount, count);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {0, 1, 0, 1, 1, 1, 1};
+        int[] arr2 = {0, 0, 1, 0, 1, 0};
+
+        System.out.println(maxConsecutiveOccurrencesApproach1(arr));
+        System.out.println(maxConsecutiveOccurrencesApproach2(arr2));
+    }
+}
